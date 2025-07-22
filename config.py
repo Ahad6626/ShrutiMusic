6ebda7e300fc85e2c80e81fd56f72dfedd46689b
Ahad6626 <asad575530@gmail.com>
2025-07-22 21:49:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ load_dotenv()
 # 📲 Telegram & API Credentials
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-API_ID = int(os.getenv("API_ID","28209312")
+API_ID = int(getenv("API_ID","28209312"))
 API_HASH = os.getenv("API_HASH","89def84a7894bb696ff20174c86889a4")
 BOT_TOKEN = os.getenv("BOT_TOKEN","7408662813:AAFVjRXCtR3xxzRqLo43MS6jB2Lrt07zCjU")
 OWNER_ID = int(os.getenv("OWNER_ID", "7740205974"))
