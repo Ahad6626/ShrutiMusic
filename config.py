dc30284076dc1926f40aef21c41c47d947bee81f
Ahad6626 <asad575530@gmail.com>
2025-08-09 02:03:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,9 +87,9 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/eehxb4.jpg")
-PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/eehxb4.jpg")
+PING_IMG_URL = os.getenv("PING_IMG_URL", "https://graph.org/file/e4006465ee3a02eb03e34-3e3c75ce0b8eba1698.jpg")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+STATS_IMG_URL = "https://graph.org/file/f2c3d24c4feb57a523520-e6c4ce9513b7f1c650.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
