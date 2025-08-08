427f7168a6fff3c35f30c5a90bcc120bde617b1c
Ahad6626 <asad575530@gmail.com>
2025-08-09 01:58:44 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -9,11 +9,7 @@ def start_panel(_):
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-        ],
-        [
-            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"),
-            InlineKeyboardButton(text="˹𝐏ʀɪᴠᴀᴄʏ 𝐏ᴏʟɪᴄʏ˼", url="https://graph.org/Privacy-Policy-05-01-30")
-        ],
+        ],             
     ]
     return buttons
 
@@ -26,11 +22,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"),
-            InlineKeyboardButton(text="˹𝐏ʀɪᴠᴀᴄʏ 𝐏ᴏʟɪᴄʏ˼", url="https://graph.org/Privacy-Policy-05-01-30")
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+                       InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
